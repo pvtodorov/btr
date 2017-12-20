7b9a6d9899fdab115414d59fe48a6560c69840b6
Petar Todorov <petar.v.todorov@gmail.com>
2017-12-19 19:48:49 -0500

    Better documentation of how the script functions

--- predict_background.py
+++ predict_background.py
@@ -9,23 +9,33 @@ import os
 
 
 def get_data_cols(df, meta_cols):
+    """ Given a dataframe and its meta columns, get back a list of the data
+    columns from the dataframe.
+    """
     cols = df.columns.tolist()
     data_cols = [x for x in cols if x not in meta_cols]
     return data_cols
 
 
 def sample_data_cols(data_cols, k):
+    """ Select a random sample of k-items from a list of columns.
+    """
     sampled_cols = random.sample(data_cols, k)
     return [x for x in data_cols if x in sampled_cols]
 
 
 def get_X_y(df, target, data_cols):
+    """ Given a dataframe, a target col, and a sample of data column names,
+    generate X, an numpy array of the data, and y, a list of target values
+    corresponding to each row.
+    """
     X = df.as_matrix(columns=data_cols)
     y = df[target].tolist()
     return X, y
 
 
 def fit_RF(X, y):
+    """ Fit and return a randomForestRegressor object to the provided data """
     rf = RandomForestRegressor(n_estimators=100,
                                max_features='auto',
                                n_jobs=4,
@@ -37,6 +47,11 @@ def fit_RF(X, y):
 def gen_background_performance(df, target, data_cols,
                                interval=10,
                                max_cols=500):
+    """ Given a dataframe, a target variable, data columns, a max number of
+    columns to fit, and an interval to increase the sample of columns. Fits
+    the data over the intervals and returns the oob R2 scores for each fit
+    as a dict of {n_features: R2, ...}
+    """
     oob_scores = {}
     for k in tqdm(range(10, max_cols, interval)):
         selected_cols = sample_data_cols(data_cols, k)
@@ -47,6 +62,8 @@ def gen_background_performance(df, target, data_cols,
 
 
 def save_oob_scores(oob_scores, folder):
+    """ Given a dict of oob_scores and a folder path, dump a csv of the
+    oob_scores into the folder """
     d = oob_scores
     scores = pd.DataFrame(list(d.items()), columns=['n_features', 'R2'])
     scores = scores.sort_values('n_features').reset_index(drop=True)
@@ -74,8 +91,11 @@ if __name__ == '__main__':
     target = settings['target']
     subset = settings['subset']
 
+    # make sure df target variable doesn't have NaNs
     df = df[(df[target].notnull())]
+    # select a particular subset of the dataset based on BrodmannArea
     df = df[df['BrodmannArea'] == subset]
+    # drop duplicate IDs, taking only the first time the ID occurs
     df = df.drop_duplicates(subset='ID', keep='first')
     data_cols = get_data_cols(df, settings['meta_cols'])
     print(df.shape)
