41415f4bc0a1714a9e4d1b7b1dd5ebcebbf86966
pvtodorov <petar.v.todorov@gmail.com>
2018-01-05 09:56:47 -0500

    Predict background (#2)
    * Initial commit of background predicting code
    
    - Two files: predict_background.py and settings.json
    - predict_background.py will take two command line arguments, a settings
      file path and a number of iterations it should run for
    - the settings file contains parameters such as the infile, outfolder,
      a list of meta columns in the infile, the target variable,
      and the subset that should be selected
    - when called from the command line, the script will for as many
      iterations as requested, dumping the R2 oob score for 10 to max_cols
      in the specified interval. These will be saved as csvs in the
      outfolder. Each csv has a unique name (as generated by Python's uuid
      library) so the function can be run on many machines without fear
      of stomping its own output.
    
    * Better documentation of how the script functions
    
    * Add requirements file that lists dependencies
    
    * Aggregate background runs
    
    - take the oob scores from background runs (referenced to by the
      settings file) and produce one csv of all the runs
    
    * First commit of predict_set
    
    - given a background of predictions (R2) from RF models, test a gmt or
      txt gene list file for its performance and report R2, p_value,
      and adjusted p_value (Benjamini-Hochberg FDR) if multiple predictions
      are made
    
    * Add progress bar to predict_set
    
    * Skip prediction if no gene set overlap in predict_set
    
    - otherwise the RF will throw an error
    
    * Select proper background 
    
    - previously all gene sets selected a background of 10 genes 
    
    `b_idx = (np.abs(np.array(bcg_cols) - 1)).argmin()`
    should be
    `b_idx = (np.abs(np.array(bcg_cols) - n_genes)).argmin()`
    
    * Changes to dataframe sorting, naming
    
    * Extend background prediction from 500 -> 1000 genes
    
    * Fix loop responsible for background prediction
    
    - previously would iterate over the entire range and miss the last size in the range
    - i.e. stopping at 490 genes when 500 requested
    
    * Don't print things from predict_set
    
    - useful for debugging, more of a clutter now. removed `print()`
    
    * Fix bug in predict_set
    
    introduced bug when getting rid of print statements, left an empty 'if' statement
    causing program to error
    
    * Results file now reports how many genes from set are used
    
    - new columns `n_genes` for length of original gene list and `intersect` for 
      length of intersect between gene set and columns in msbb file
    
    * remove get_gmt_dict
    
    - vestigial function, unnecessary
    
    * Sort columns in a sensible way
    
    * separate functions out of scripts
    
    * Document new functions
    
    * read_gmt handles gmt files or dir of txt gene lists
    
    - read_gmt handles gmt files or a directory containing txt files of gene lists separated
      by newlines that it assembles into one gmt
    - also returns a suffix that identifies the gmt list. in the case of the gmt file, this is
      the filename without the extension. in the case of a dir of gene lists, this is the dir
      name
    
    * Update requirements.txt
    
    * Changes to README.md
    
    - describe how to setup an env and install the required packages using
    - describe background prediction
    - describe background aggregation
    - describe set prediction

--- README.md
+++ README.md
@@ -14,3 +14,56 @@ The datasets used by the analyses in this repository can be easily downloaded us
     Rscript msbb.R <path to data>
 
 If `<path to data>` is left blank, then `/data/AMP-AD/MSBB` will be used by default.
+
+## Set up a python env
+Make sure `virtualenv` is installed. If not, install it via pip.
+    
+    pip install virtualenv
+
+Make a new virtualenv. I named my ampad-env
+    
+    virtualenv ampad-env
+
+Activate the env
+    
+    activate ampad-env
+
+Install the packages we need
+    
+    pip install -r requirements.txt
+
+
+## Obtain background predictions
+We will use a random forest regression to predict the Braak score of of samples
+based on the gene expression profile in the BM36 region. This region was picked
+because it has multiple stages of the disease. The background will be generated
+for gene sets of 10 to 1000, moving in steps of 10 randomly selected genes, where models will be
+trained on the data and the R2 out-of-bag score will be recorded as the performance.
+This will be repeated at least 10,000 times.
+
+To perform this, run the `predict_background.py` script in the command line.
+The script will run for the specified number of repetitions and dump a csv file
+for each one.
+
+    python predict_background.py <settings.json> <number of repetitions>
+
+
+## Aggregate background runs
+The repeated predictions will need to be aggregated into a single file.
+
+    python aggregate_predictions <settings.json>
+
+
+## Predict the preformance of gene sets
+To predict the performance of a gene set, obtain the necessary .gmt file or
+create a folder with .txt files which contain gene sets formatted as HGNC ID on
+each line. These can then be supplied to the `predict_set.py` script as follows:
+
+    python predict_set.py <settings.json> <path to gmt file or folder of txts>
+
+This will output a csv file with the name `scores_<gmt_file or folder suffix>.csv`
+The output will contain an identifier for the set, a description, the number of genes
+in the set, the number of genes which were actually used (if there is incomplete
+overlap between the dataset and the gene set not all genes are used), the R2 value,
+the p_value, and an Benjamini-Hochberg adjusted p_value.
+
---
+++ msbb_functions.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import random
+from sklearn.ensemble import RandomForestRegressor
+import uuid
+from tqdm import tqdm
+import csv
+import os
+
+
+def get_data_cols(df, meta_cols):
+    """ Given a dataframe and its meta columns, get back a list of the data
+    columns from the dataframe.
+    """
+    cols = df.columns.tolist()
+    data_cols = [x for x in cols if x not in meta_cols]
+    return data_cols
+
+
+def sample_data_cols(data_cols, k):
+    """ Select a random sample of k-items from a list of columns.
+    """
+    sampled_cols = random.sample(data_cols, k)
+    return [x for x in data_cols if x in sampled_cols]
+
+
+def get_X_y(df, target, data_cols):
+    """ Given a dataframe, a target col, and a sample of data column names,
+    generate X, an numpy array of the data, and y, a list of target values
+    corresponding to each row.
+    """
+    X = df.as_matrix(columns=data_cols)
+    y = df[target].tolist()
+    return X, y
+
+
+def fit_RF(X, y):
+    """ Fit and return a randomForestRegressor object to the provided data """
+    rf = RandomForestRegressor(n_estimators=100,
+                               max_features='auto',
+                               n_jobs=4,
+                               oob_score=True)
+    rf.fit(X, y)
+    return rf
+
+
+def gen_background_performance(df, target, data_cols,
+                               interval=10,
+                               max_cols=1000):
+    """ Given a dataframe, a target variable, data columns, a max number of
+    columns to fit, and an interval to increase the sample of columns. Fits
+    the data over the intervals and returns the oob R2 scores for each fit
+    as a dict of {n_features: R2, ...}
+    """
+    oob_scores = {}
+    for k in tqdm(range(10, max_cols + interval, interval)):
+        selected_cols = sample_data_cols(data_cols, k)
+        X, y = get_X_y(df, target, selected_cols)
+        rf = fit_RF(X, y)
+        oob_scores[k] = rf.oob_score_
+    return oob_scores
+
+
+def save_oob_scores(oob_scores, folder):
+    """ Given a dict of oob_scores and a folder path, dump a csv of the
+    oob_scores into the folder """
+    d = oob_scores
+    scores = pd.DataFrame(list(d.items()), columns=['n_features', 'R2'])
+    scores = scores.sort_values('n_features').reset_index(drop=True)
+    outfile = str(uuid.uuid4())
+    scores.to_csv(folder + '/' + outfile + '.csv', index=False)
+
+
+def get_gene_list_intersect(gene_list, data_cols):
+    """ return the intersection between the current gene list HGNC symbols and
+    the columns in the dataset. return a second list, `missing` for any genes
+    that are missing.
+    """
+    intersect = [x for x in gene_list if x in data_cols]
+    missing = [x for x in gene_list if x not in data_cols]
+    return intersect, missing
+
+
+def standardize_gmt(gmt):
+    """ Takes a loaded list from a .gmt file and reformats it, if necessary,
+    so that the html id is always at index 0 and the description is at index 1
+    """
+    if 'http' in gmt[0][1]:
+        gmt_standard = [[x[1]] + [x[0]] + x[2:] for x in gmt]
+    else:
+        gmt_standard = gmt
+    return gmt_standard
+
+
+def read_gmt(path):
+    """ given a filepath, reads the gmt or txt file at that location, returning
+    a list that can be used in the scripts
+    """
+    if os.path.isfile(path):
+        gmt = []
+        with open(path) as f:
+            rd = csv.reader(f, delimiter="\t", quotechar='"')
+            for row in rd:
+                gmt.append(row)
+        gmt = standardize_gmt(gmt)
+        gmt_suffix = path.split('/')[-1][:-4]
+        return gmt, gmt_suffix
+    else:
+        files = os.listdir(path)
+        gmt = []
+        gmt_suffix = path.split('/')[-2]
+        for f in files:
+            print(f)
+            with open(path + f) as fd:
+                rd = csv.reader(fd, delimiter="\t", quotechar='"')
+                gene_list = []
+                for row in rd:
+                    gene_list.append(row[0])
+                gmt.append([f, 'user defined'] + gene_list)
+        return gmt, gmt_suffix
