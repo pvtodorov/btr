0d12c4951748047b5afbe0e008c13b9c5bb1b0ad
Unknown <petar.v.todorov@gmail.com>
2018-01-16 22:14:30 -0500

    Move gen_background_predictionst to predict_background_classify.py
    - specific to script, not general functions

--- predict_background_classify.py
+++ predict_background_classify.py
@@ -4,6 +4,27 @@ import os
 from msbb_functions import *
 
 
+def gen_background_predictions(df, target, data_cols,
+                               subset_col, subsets,
+                               interval=10, max_cols=1000):
+    """ Generate binary classification predictions accross subsets.
+    """
+    bcg_predictions = recursivedict()
+    for subset in subsets:
+        df_train, df_test = get_train_test_df(df, subset_col, [subset])
+        for k in tqdm(range(10, max_cols + interval, interval)):
+            selected_cols = sample_data_cols(data_cols, k)
+            X_train, y_train = get_X_y(df_train, target, selected_cols)
+            y_train = binarize_Braak_scores(y_train)
+            X_test, y_test = get_X_y(df_test, target, selected_cols)
+            rf = fit_RF(X_train, y_train, 'classification')
+            predictions = predict_RF(rf, X_test)
+            sub_ids = df_test['ID'].tolist()
+            for id, p in zip(sub_ids, predictions):
+                bcg_predictions[subset[0]][id][k] = p
+    return bcg_predictions
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("settings_path", help="settings and parameters json")
