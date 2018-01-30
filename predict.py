398932bf799a0cb3fc80c61c50472359b6cd7192
Unknown <petar.v.todorov@gmail.com>
2018-01-30 01:00:48 -0500

    a script to do all the predicting
    - predict.py will take a settings_path, and a gmt_path, (iterations will be added later)
    - the settings path tells the Loader what Processor to create
    - the processor is set to generate background predictions by default, if a gmt_path
      is supplied, the processor will generate predictions for the gmt instead

---
+++ predict.py
@@ -0,0 +1,23 @@
+import argparse
+from processing_schemes import Loader
+from gmt import GMT
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings_path", help="settings as JSON")
+    parser.add_argument("-i", "--iterations", help="iterations for the script",
+                        required=False, default="1")
+    parser.add_argument("-g", "--gmt_path",
+                        help="path to file or folder of txts", required=False)
+    argument = parser.parse_args()
+    args = parser.parse_args()
+    settings_path = args.settings_path
+    iterations = int(args.iterations)
+    gmt_path = args.gmt_path
+    gmt = None
+    if gmt_path:
+        gmt = GMT(gmt_path)
+    proc = Loader.processor_from_settings(settings_path=settings_path)
+    proc.from_settings()
+    proc.predict_background()
+    proc.save_results(gmt)
