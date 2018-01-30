6dd03c1803640646678c7524e603a56d3685d9e0
Unknown <petar.v.todorov@gmail.com>
2018-01-30 00:57:57 -0500

    fixes and additions to modular pipeline
    - Processing schemes has a loader class that loads settings from a path and returns
      the Processor subclass requested in the settings file (LPOCV in this case)
    - added some code to save results and generate the folders for those results
    - reflected changes in settings_standard.json as well

--- processing_schemes.py
+++ processing_schemes.py
@@ -3,45 +3,112 @@ import numpy as np
 from tqdm import tqdm
 from itertools import combinations
 from msbb_functions import *
+import uuid
+import json
+from dataset import Dataset
+from estimators import get_estimator
 
 
-class LPOCV(object):
-    def __init__(self, dataset, settings, estimator):
-        self._d = dataset
-        self._s = settings
-        self._e = estimator
-        self._sampling_range = get_sampling_range(settings)
+class Loader(object):
+    @staticmethod
+    def processor_from_settings(settings_path):
+        if settings_path:
+            with open(settings_path) as f:
+                settings = json.load(f)
+        name = settings["processing_scheme"]["name"]
+        if name == 'LPOCV':
+            return LPOCV(settings=settings)
+        else:
+            raise NotImplementedError
+
+
+class Processor(object):
+    def __init__(self, settings=None, dataset=None, estimator=None):
+        self.s = None
+        if settings:
+            self.s = settings
+        self.d = None
+        if dataset:
+            self.d = dataset
+        self.e = None
+        if estimator:
+            self.e = estimator
+
+    def from_settings(self, settings_path=None):
+        if settings_path:
+            with open(settings_path) as f:
+                settings = json.load(f)
+            self.s = settings
+        if self.s:
+            self.d = Dataset(self.s)
+            self.e = get_estimator(self.s)
+
+
+class LPOCV(Processor):
+    def __init__(self, settings=None, dataset=None, estimator=None):
+        super().__init__(settings=settings,
+                         dataset=dataset,
+                         estimator=estimator)
+        self.selected_pairs = []
+        self.df_result = pd.DataFrame()
         self._bcg_predictions = recursivedict()
         self._pairs_list = []
         self._transform = settings["processing_scheme"].get("transform_labels")
-        self.selected_pairs = []
-        self.df_result = pd.DataFrame()
-        self.file_path = ''
-        self._get_pairs(shuffle=True,
-                        seed=self._s["misc"]["prng_seed"],
-                        sample_once=False)
+        self._outdir_path = ''
+        self._outfile_name = ''
 
     def predict_background(self):
-        for k in tqdm(self._sampling_range):
-            gene_list = self._d.sample_data_cols(k)
+        sampling_range = get_sampling_range(self.s)
+        for k in tqdm(sampling_range):
+            gene_list = self.d.sample_data_cols(k)
             self._build_bcg_predictions(gene_list, k)
-            self._build_df_result()
+        self._build_df_result()
 
     def predict_gmt(self, gmt):
-        for link, _, gene_list, _ in tqdm(gmt.generate(self._d.data_cols)):
+        for link, _, gene_list, _ in tqdm(gmt.generate(self.d.data_cols)):
             self._build_bcg_predictions(gene_list, link)
-            self._build_df_result()
+        self._build_df_result()
+
+    def save_results(self, gmt=None):
+        self._get_outdir_path(gmt)
+        check_or_create_dir(self._outdir_path)
+        self._get_outfile_name(gmt)
+        results_path = self._outdir_path + self._outfile_name
+        self.df_result.to_csv(results_path, index=False)
+
+    def _get_outdir_path(self, gmt):
+        dset_name = self.s["dataset"]["name"]
+        scheme_name = self.s["processing_scheme"]["name"]
+        subset_name = self.s["processing_scheme"]["subset"]
+        est_name = self.s["estimator"]["name"]
+        prediction_type = "background_predictions"
+        if gmt:
+            prediction_type = "geneset_predictions"
+        outdir_path = (dset_name + '/' +
+                       scheme_name + '/' + subset_name + '/' +
+                       est_name + '/' +
+                       prediction_type + '/')
+        self._outdir_path = outdir_path
+
+    def _get_outfile_name(self, gmt):
+        if gmt:
+            outfile_name = gmt.suffix
+        else:
+            outfile_name = str(uuid.uuid4())
+        outfile_name = outfile_name + '.csv'
+        self._outfile_name = outfile_name
 
     def _build_bcg_predictions(self, selected_cols, k):
+        self._get_pairs(**self.s["processing_scheme"]["pair_settings"])
         for pair_index, pair in enumerate(tqdm(self.selected_pairs)):
                 pair_ids = (pair[0][0], pair[1][0])
-                train_test_list = self._d.get_X_y(pair_ids,
-                                                  selected_cols=selected_cols)
+                train_test_list = self.d.get_X_y(pair_ids,
+                                                 selected_cols=selected_cols)
                 X_train, y_train, X_test, _ = train_test_list
                 y_train = [int(x) for x in y_train]
                 y_train = np.array(y_train)
                 y_train = digitize_labels(y_train, self._transform)
-                e = self._e()
+                e = self.e
                 e = e.fit(X_train, y_train)
                 predictions = e.predict(X_test)
                 for i_s, p in zip(pair_ids, predictions):
@@ -63,8 +130,8 @@ class LPOCV(object):
 
     def _build_pairs_list(self):
         """ generate pairs of samples """
-        dataset = self._d
-        settings = self._s
+        dataset = self.d
+        settings = self.s
         transform = self._transform
         subset_column = settings["processing_scheme"]["subset_col"]
         subset = settings["processing_scheme"]["subset"]
@@ -83,6 +150,7 @@ class LPOCV(object):
     def _get_pairs(self, shuffle=True, seed=47, sample_once=False):
         """ Added function to return a set of pairs that have each sample
         in data at least once """
+        self.selected_pairs = []
         if len(self._pairs_list) == 0:
             self._build_pairs_list()
         sample_list = sorted(list(set([sample[0] for pair
