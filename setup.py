59b1f23277a2d5d797658d71f4efc2cc5ef49a15
Unknown <petar.v.todorov@gmail.com>
2018-02-13 13:44:56 -0500

    first commit of setup.py file
    - used by setuptools/pip to install package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='btr',
+      version='0.1',
+      description='better than random comparisons made easy',
+      url='http://github.com/pvtodorov/btr',
+      author='Petar Todorov, Artem Sokolov',
+      author_email='petar.v.todorov@gmail.com',
+      license='MIT',
+      packages=['btr'],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': ['btr-predict=btr.command_line:predict_main',
+                              'btr-score=btr.command_line:score_main',
+                              'btr-stats=btr.command_line:stats_main']
+      }
+    )
