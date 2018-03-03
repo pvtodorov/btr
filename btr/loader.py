0667a2e00b4ba40bf3f4a47ab7e21bd060c5114c
Unknown <petar.v.todorov@gmail.com>
2018-03-03 15:53:57 -0500

    loader updated
    - loader will be the point of entry for synapse
    - will store settings, processors, synapse client, (noow) and 
      datasets, gmts, etc (in the future)
    - can now save files with annotations directly to synapse via
      `save_prediction_to_synapse`
    - implemented helper function `get_or_create_syn_folder` to get to the right
      parent folder, either by creating it or getting it from synapse

--- btr/loader.py
+++ btr/loader.py
@@ -1,18 +1,57 @@
 import json
 from .processing_schemes import LPOCV
+import synapseclient
+import synapseutils
+from synapseclient import File, Folder
+from .utilities import get_settings_annotations
 
 
 class Loader(object):
     def __init__(self, settings_path=None):
         self.s = None
+        self.proc = None
+        self._syn = None
         if settings_path:
             with open(settings_path) as f:
                 self.s = json.load(f)
 
-    def processor_from_settings(self):
+    def get_processor_from_settings(self):
         settings = self.s
         name = settings["processing_scheme"]["name"]
         if name == 'LPOCV':
-            return LPOCV(settings=settings)
+            self.proc = LPOCV(settings=settings)
         else:
             raise NotImplementedError
+
+    def save_prediction_to_synapse(self):
+        """Saves prediction to Synapse"""
+        self._syn = synapseclient.login()
+        dirpath = self.proc._outdir_path
+        filename = self.proc._outfile_name
+        parent = get_or_create_syn_folder(self._syn,
+                                          dirpath,
+                                          self.s['project_synid'])
+        file = File(path=dirpath + filename, parent=parent)
+        annotations = get_settings_annotations(self.s)
+        if 'background_predictions' in dirpath:
+            annotations['prediction_type'] = 'background'
+        elif "hypothesis_predictions" in dirpath:
+            annotations['prediction_type'] = "hypothesis"
+        else:
+            raise NotImplementedError
+        gmt = self.proc.gmt
+        if gmt:
+            annotations['gmt'] = gmt.suffix
+        file.annotations = annotations
+        file = self._syn.store(file)
+
+
+def get_or_create_syn_folder(syn, dirpath, project_synid):
+    dirs = [x for x in dirpath.split('/') if len(x) > 0]
+    parent_obj = syn.get(project_synid)
+    while len(dirs) > 0:
+        d = dirs.pop(0)
+        folder = Folder(d, parent=parent_obj)
+        folder = syn.store(folder)
+        parent_obj = folder
+    return parent_obj
