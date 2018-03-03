cad608f5d9c7ed66566a2f301e2a4b233dd27de0
Unknown <petar.v.todorov@gmail.com>
2018-03-03 15:10:03 -0500

    produce annotations from settings
    - basic annotations generated from settings for each file
    - additional annotations to be generated depending on what is done, but these
      will be in every prediction

--- btr/utilities.py
+++ btr/utilities.py
@@ -58,3 +58,67 @@ def digitize_labels(y, transform_thresholds):
     """
     y_bin = np.digitize(y, transform_thresholds) - 1
     return y_bin
+
+
+def get_settings_annotations(settings):
+    annotations = {}
+    package_path = importlib.util.find_spec('btr').origin[:-15]
+    origin = subprocess.check_output(["git", "-C", package_path, "config",
+                                      "--get", "remote.origin.url"]).decode()
+    annotations['github_origin'] = origin
+    commit_hash = subprocess.check_output(["git", "-C", package_path,
+                                           "rev-parse", "HEAD"])
+    commit_hash = commit_hash.strip().decode()
+    annotations['github_commit_hash'] = commit_hash
+    annotations['dataset_name'] = settings['dataset']['name']
+    annotations['dataset_filepath'] = settings['dataset']['filepath']
+    annotations['dataset_meta_columns'] = settings['dataset']['meta_columns']
+    annotations['dataset_target'] = settings['dataset']['target']
+    annotations['dataset_ID_column'] = settings['dataset']['ID_column']
+    annotations['dataset_filter_name'] = json.dumps(False)
+    if settings['dataset'].get('filter'):
+        annotations['dataset_filter_name'] = \
+            settings['dataset']['filter']['name']
+        annotations['dataset_filters_num'] = \
+            len(settings['dataset']['filter']['filters'])
+        for i, f in enumerate(settings['dataset']['filter']['filters']):
+            annotations['dataset_filter_column_' + str(i)] = f['column']
+            annotations['dataset_filter_values_' + str(i)] = \
+                "".join(json.dumps(f['values']))
+    annotations['estimator_name'] = \
+        settings['estimator']['name']
+    annotations['estimator_params'] = \
+        "".join(json.dumps(settings['estimator']['estimator_params']).split())
+    annotations['processing_scheme_name'] = \
+        settings['processing_scheme']['name']
+    if annotations['processing_scheme_name'] == 'LPOCV':
+        annotations['processing_scheme_subset_col'] = \
+            settings['processing_scheme']['subset_col']
+        annotations['processing_scheme_subset'] = \
+            settings['processing_scheme']['subset']
+        annotations['processing_scheme_pair_col'] = \
+            settings['processing_scheme']['pair_col']
+        annotations['processing_scheme_transform_labels'] = \
+            "".join(json.dumps(settings['processing_scheme']
+                                       ['transform_labels']))
+        annotations['processing_scheme_pair_settings_shuffle'] = \
+            json.dumps(settings['processing_scheme']
+                               ['pair_settings']
+                               ['shuffle'])
+        annotations['processing_scheme_pair_settings_seed'] = \
+            settings['processing_scheme']['pair_settings']['seed']
+        annotations['processing_scheme_pair_settings_sample_once'] = \
+            json.dumps(settings['processing_scheme']
+                               ['pair_settings']
+                               ['sample_once'])
+    annotations['background_params_intervals'] = \
+        len(settings['background_params']['intervals'])
+    for i, p in enumerate(settings['background_params']['intervals']):
+        annotations['background_params_interval_start_' + str(i)] = \
+            p['start']
+        annotations['background_params_interval_end_' + str(i)] = \
+            p['end']
+        annotations['background_params_interval_step_' + str(i)] = \
+            p['step']
+    annotations['misc'] = "".join(json.dumps(settings['misc']))
+    return annotations
