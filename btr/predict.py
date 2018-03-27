295f57d4ffe3c48b7f4663e0a1018580323266bd
Unknown <petar.v.todorov@gmail.com>
2018-03-27 12:10:29 -0400

    user can specify overwrite (or not) for settings on synapse with predict
    -  will be useful when testing the commandline implementation (not yet)

--- btr/predict.py
+++ btr/predict.py
@@ -10,15 +10,20 @@ def main():
                         required=False, default="1")
     parser.add_argument("-g", "--gmt_path",
                         help="path to file or folder of txts", required=False)
+    parser.add_argument("-o", "--overwrite_settings",
+                        help="path to file or folder of txts", required=False,
+                        action='store_true')
     args = parser.parse_args()
     settings_path = args.settings_path
     iterations = int(args.iterations)
+    overwrite_settings = args.overwrite_settings
     gmt_path = args.gmt_path
     gmt = None
     for i in range(0, iterations):
         if gmt_path:
             gmt = GMT(gmt_path)
-        loader = Loader(settings_path=settings_path)
+        loader = Loader(settings_path=settings_path, use_synapse=True,
+                        syn_settings_overwrite=overwrite_settings)
         loader.get_processor_from_settings()
         loader.proc.from_settings()
         loader.proc.predict(gmt=gmt)
