e33395999bc3cb6e0073e26113ba5cccaeb6877f
Unknown <petar.v.todorov@gmail.com>
2018-03-28 10:57:23 -0400

    condense commandline tests to two
    - now get their file paths from a list

--- btr/tests/test_commandline.py
+++ btr/tests/test_commandline.py
@@ -1,58 +1,29 @@
 from subprocess import run, PIPE, STDOUT
 
-
-def test_predict_synthetic_Ordinal():
-    s = run(['btr-predict',
-             'test_data/run_settings/settings_synthetic_Ordinal.json',
-             '-g', 'test_data/hypotheses/synthetic/',
-             '-o'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
+settings_files = [
+    'test_data/run_settings/settings_synthetic_Ordinal.json',
+    'test_data/run_settings/settings_synthetic_Multiclass_Linear.json',
+    'test_data/run_settings/settings_synthetic_Multiclass_Nonlinear.json']
 
 
-def test_predict_synthetic_Multiclass_Linear():
-    s = run(['btr-predict',
-             'test_data/run_settings/settings_synthetic_Multiclass_Linear.json',
-             '-g', 'test_data/hypotheses/synthetic/',
-             '-o'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
+def test_predict_synthetic():
+    for s in settings_files:
+        s = run(['btr-predict',
+                 s,
+                 '-g', 'test_data/hypotheses/synthetic/',
+                 '-o'],
+                stdout=PIPE,
+                stderr=STDOUT)
+        assert s.returncode == 0
 
 
-def test_predict_synthetic_Multiclass_Nonlinear():
-    s = run(['btr-predict',
-             'test_data/run_settings/settings_synthetic_Multiclass_Nonlinear.json',
-             '-g', 'test_data/hypotheses/synthetic/',
-             '-o'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
+def test_score_synthetic():
+    for s in settings_files:
+        s = run(['btr-score',
+                 'test_data/run_settings/settings_synthetic_Ordinal.json',
+                 '-g', 'test_data/hypotheses/synthetic/'],
+                stdout=PIPE,
+                stderr=STDOUT)
+        assert s.returncode == 0
 
 
-def test_score_synthetic_Ordinal():
-    s = run(['btr-score',
-             'test_data/run_settings/settings_synthetic_Ordinal.json',
-             '-g', 'test_data/hypotheses/synthetic/'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
-
-
-def test_score_synthetic_Multiclass_Linear():
-    s = run(['btr-score',
-             'test_data/run_settings/settings_synthetic_Multiclass_Linear.json',
-             '-g', 'test_data/hypotheses/synthetic/'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
-
-
-def test_score_synthetic_Multiclass_Nonlinear():
-    s = run(['btr-score',
-             'test_data/run_settings/settings_synthetic_Multiclass_Nonlinear.json',
-             '-g', 'test_data/hypotheses/synthetic/'],
-            stdout=PIPE,
-            stderr=STDOUT)
-    assert s.returncode == 0
