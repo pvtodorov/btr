53c6d0f7412eca8b1be86bd222b94924c159e46c
Unknown <petar.v.todorov@gmail.com>
2018-05-13 18:04:30 -0400

    Dataset tests update
    - limited settings files as outlined in the last two commits
    - new dataset loader `load_dataset_digitized_str2float_confounder` which loads
      the dataset with a confounder attribute
    - updated `dataset_loads` to be iterated over in tests

--- btr/tests/test_dataset.py
+++ btr/tests/test_dataset.py
@@ -3,13 +3,12 @@ from nose.tools import assert_raises
 
 
 def load_dataset_basic():
-    settings = {"dataset": {
-        "name": "synthetic_test_data",
-        "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
-        "meta_columns": ["ID", "PMI", "AOD", "CDR",
-                         "Braak", "BrodmannArea", "Barcode"],
-        "target": "Braak",
-        "ID_column": "ID"}}
+    settings = {"name": "synthetic_test_data",
+                "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
+                "meta_columns": ["ID", "PMI", "AOD", "CDR",
+                                 "Braak", "BrodmannArea", "Barcode"],
+                "target": "Braak",
+                "ID_column": "ID"}
     dataset = Dataset(settings)
     print(dataset)
     return dataset
@@ -17,69 +16,85 @@ def load_dataset_basic():
 
 def load_dataset_filtered_AC(transform_dataset=True):
     values = [0, 1, 2, 5, 6]
-    settings = {"dataset": {
-        "name": "synthetic_test_data",
-        "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
-        "meta_columns": ["ID", "PMI", "AOD", "CDR",
-                         "Braak", "BrodmannArea", "Barcode"],
-        "target": "Braak",
-        "ID_column": "ID",
-        "transforms": [{"operation": "filter",
-                        "column": "Braak",
-                        "values": values,
-                        "name": "AC"}]}}
+    settings = {"name": "synthetic_test_data",
+                "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
+                "meta_columns": ["ID", "PMI", "AOD", "CDR",
+                                 "Braak", "BrodmannArea", "Barcode"],
+                "target": "Braak",
+                "ID_column": "ID",
+                "transforms": [{"operation": "filter",
+                                "column": "Braak",
+                                "values": values,
+                                "name": "AC"}]}
     dataset = Dataset(settings, transform_dataset=transform_dataset)
     return dataset
 
 
 def load_dataset_digitized(transform_dataset=True):
-    settings = {"dataset": {
-        "name": "synthetic_test_data",
-        "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
-        "meta_columns": ["ID", "PMI", "AOD", "CDR",
-                         "Braak", "BrodmannArea", "Barcode"],
-        "target": "Braak",
-        "ID_column": "ID",
-        "transforms": [{"operation": "digitize",
-                        "column": "Braak",
-                        "thresholds": [0, 3, 5]}]}}
+    settings = {"name": "synthetic_test_data",
+                "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
+                "meta_columns": ["ID", "PMI", "AOD", "CDR",
+                                 "Braak", "BrodmannArea", "Barcode"],
+                "target": "Braak",
+                "ID_column": "ID",
+                "transforms": [{"operation": "digitize",
+                                "column": "Braak",
+                                "thresholds": [0, 3, 5]}]}
     dataset = Dataset(settings, transform_dataset=transform_dataset)
     return dataset
 
 
 def load_dataset_digitized_str2float(transform_dataset=True):
-    settings = {"dataset": {
-        "name": "synthetic_test_data",
-        "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
-        "meta_columns": ["ID", "PMI", "AOD", "CDR",
-                         "Braak", "BrodmannArea", "Barcode"],
-        "target": "Braak",
-        "ID_column": "ID",
-        "transforms": [{"operation": "digitize",
-                        "column": "Braak",
-                        "thresholds": [0, 3, 5]},
-                       {"operation": "str2float",
-                        "columns_list": ['AOD']}]}}
+    settings = {"name": "synthetic_test_data",
+                "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
+                "meta_columns": ["ID", "PMI", "AOD", "CDR",
+                                 "Braak", "BrodmannArea", "Barcode"],
+                "target": "Braak",
+                "ID_column": "ID",
+                "transforms": [{"operation": "digitize",
+                                "column": "Braak",
+                                "thresholds": [0, 3, 5]},
+                               {"operation": "str2float",
+                                "columns_list": ['AOD']}]}
     dataset = Dataset(settings, transform_dataset=transform_dataset)
+    return dataset
+
+
+def load_dataset_digitized_str2float_confounder(transform_dataset=True):
+    settings = {"name": "synthetic_test_data",
+                "filepath": "~/Code/btr/btr/tests/test_data/synthetic.tsv",
+                "meta_columns": ["ID", "PMI", "AOD", "CDR",
+                                 "Braak", "BrodmannArea", "Barcode"],
+                "target": "Braak",
+                "ID_column": "ID",
+                "confounder": "AOD",
+                "transforms": [{"operation": "digitize",
+                                "column": "Braak",
+                                "thresholds": [0, 3, 5]},
+                               {"operation": "str2float",
+                                "columns_list": ['AOD']}]}
+    dataset = Dataset(settings, transform_dataset=transform_dataset,
+                      usecols=["ID", "AOD", "Braak"])
     return dataset
 
 
 dataset_loads = [load_dataset_basic,
                  load_dataset_filtered_AC,
-                 load_dataset_digitized]
+                 load_dataset_digitized,
+                 load_dataset_digitized_str2float]
 
 
 def test_dataset_name():
     for f in dataset_loads:
         dataset = f()
-        name = dataset.settings['dataset']['name']
+        name = dataset.settings['name']
         assert(dataset.name == name)
 
 
 def test_dataset_filepath():
     for f in dataset_loads:
         dataset = f()
-        filepath = dataset.settings['dataset']['filepath']
+        filepath = dataset.settings['filepath']
         assert(dataset.filepath == filepath)
 
 
@@ -87,7 +102,7 @@ def test_dataset_meta_columns():
     for f in dataset_loads:
         dataset = f()
         settings = dataset.settings
-        assert(dataset.meta_cols == settings['dataset']['meta_columns'])
+        assert(dataset.meta_cols == settings['meta_columns'])
 
 
 def test_dataset_data_columns():
@@ -95,7 +110,7 @@ def test_dataset_data_columns():
         dataset = f()
         settings = dataset.settings
         data_columns = [x for x in dataset.data.columns.tolist()
-                        if x not in settings['dataset']['meta_columns']]
+                        if x not in settings['meta_columns']]
         assert(data_columns == dataset.data_cols)
 
 
@@ -103,7 +118,7 @@ def test_dataset_target_column():
     for f in dataset_loads:
         dataset = f()
         settings = dataset.settings
-        target = settings['dataset']['target']
+        target = settings['target']
         assert(dataset.target == target)
 
 
@@ -111,7 +126,7 @@ def test_dataset_id_column():
     for f in dataset_loads:
         dataset = f()
         settings = dataset.settings
-        id_col = settings['dataset']['ID_column']
+        id_col = settings['ID_column']
         assert(dataset.id_col == id_col)
 
 
@@ -136,7 +151,7 @@ def test_target_unique_values_before_filter():
 
 def test_filter_from_settings_during_load():
     dataset = load_dataset_filtered_AC()
-    transforms = dataset.settings['dataset']['transforms']
+    transforms = dataset.settings['transforms']
     filter_params = [x for x in transforms if x.get('operation') == 'filter']
     expected_values = filter_params[0]['values']
     actual_values = dataset.data[dataset.target].unique().tolist()
@@ -149,7 +164,7 @@ def test_filter_from_settings_after_load():
     actual_values = sorted(dataset.data[dataset.target].unique().tolist())
     expected_values = [0, 1, 2, 3, 4, 5, 6]
     assert(actual_values == expected_values)
-    transforms = dataset.settings['dataset']['transforms']
+    transforms = dataset.settings['transforms']
     filter_params = [x for x in transforms if x.get('operation') == 'filter']
     expected_values = filter_params[0]['values']
     dataset.transform_dataset()
