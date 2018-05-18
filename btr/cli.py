3a9f050d6800672c773d7e20abf48a118be2ba1b
Unknown <petar.v.todorov@gmail.com>
2018-05-18 12:55:32 -0400

    scoring functional in cli.py, stats still WIP

--- btr/cli.py
+++ btr/cli.py
@@ -5,6 +5,7 @@ from .gmt import GMT
 
 
 def predict():
+    task = 'predict'
     parser = argparse.ArgumentParser()
     parser.add_argument("settings_path", help="settings as JSON")
     parser.add_argument("-i", "--iterations", help="iterations for the script",
@@ -25,18 +26,35 @@ def predict():
             gmt = GMT(gmt_path)
         loader = Loader(settings_path=settings_path, use_synapse=True,
                         syn_settings_overwrite=overwrite_settings)
-        loader.load_dataset()
-        loader.load_processor()
+        loader.load_processor(task)
         loader.proc.predict(gmt=gmt)
         loader.proc.save_results()
         loader.save_prediction_to_synapse()
+
+
+def score():
+    task = 'score'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings_path", help="settings as JSON")
+    parser.add_argument("-g", "--gmt_path",
+                        help="path to file or folder of txts", required=False)
+    args = parser.parse_args()
+    settings_path = args.settings_path
+    loader = Loader(settings_path=settings_path, use_synapse=False,
+                    syn_settings_overwrite=True)
+    loader.load_dataset(task)
+    loader.load_processor(task)
+    loader.load_gmt(args.gmt_path)
+    loader.proc.get_y_dict(loader.dataset)
+    loader.proc.score_LPOCV()
 
 
 # def stats():
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument("settings_path", help="settings as JSON")
 #     parser.add_argument("-g", "--gmt_path",
-#                         help="path to file or folder of txts", required=False)
+#                         help="path to file or folder of txts",
+#                         required=False)
 #     args = parser.parse_args()
 #     settings_path = args.settings_path
 #     gmt_path = args.gmt_path
@@ -44,16 +62,3 @@ def predict():
 #     scorer.from_settings(settings_path)
 #     scorer.score_LPOCV(gmt_path=gmt_path)
 #     scorer.get_stats(gmt_path=gmt_path)
-
-
-# def score():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("settings_path", help="settings as JSON")
-#     parser.add_argument("-g", "--gmt_path",
-#                         help="path to file or folder of txts", required=False)
-#     args = parser.parse_args()
-#     settings_path = args.settings_path
-#     gmt_path = args.gmt_path
-#     scorer = Scorer()
-#     scorer.from_settings(settings_path)
-#     scorer.score_LPOCV(gmt_path=gmt_path)
