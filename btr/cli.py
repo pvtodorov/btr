803555df1ea7aa63ec26c1140f6429549bdcf92a
Unknown <petar.v.todorov@gmail.com>
2018-06-17 19:50:17 -0400

    take out synapse stuff out of other functions, will get its own client
    - will add back later as own client
    - removed from predict/score/stats in CLI
    - removed from Loader

--- btr/cli.py
+++ btr/cli.py
@@ -1,5 +1,4 @@
 import argparse
-
 from .gmt import GMT
 from .loader import Loader
 
@@ -12,24 +11,18 @@ def predict():
                         required=False, default="1")
     parser.add_argument("-g", "--gmt_path",
                         help="path to file or folder of txts", required=False)
-    parser.add_argument("-o", "--overwrite_settings",
-                        help="path to file or folder of txts", required=False,
-                        action='store_true')
     args = parser.parse_args()
     settings_path = args.settings_path
     iterations = int(args.iterations)
-    overwrite_settings = args.overwrite_settings
     gmt_path = args.gmt_path
     gmt = None
     for i in range(0, iterations):
         if gmt_path:
             gmt = GMT(gmt_path)
-        loader = Loader(settings_path=settings_path, use_synapse=True,
-                        syn_settings_overwrite=overwrite_settings)
+        loader = Loader(settings_path=settings_path)
         loader.load_processor(task)
         loader.proc.predict(gmt=gmt)
         loader.proc.save_results()
-        loader.save_prediction_to_synapse()
 
 
 def score():
@@ -40,13 +33,12 @@ def score():
                         help="path to file or folder of txts", required=False)
     args = parser.parse_args()
     settings_path = args.settings_path
-    loader = Loader(settings_path=settings_path, use_synapse=False,
-                    syn_settings_overwrite=True)
+    loader = Loader(settings_path=settings_path, use_synapse=False)
     loader.load_dataset(task)
     loader.load_processor(task)
     loader.load_gmt(args.gmt_path)
     loader.proc.get_y_dict(loader.dataset)
-    loader.proc.score_LPOCV()
+    loader.proc.get_score()
 
 
 # def stats():
--- btr/loader.py
+++ btr/loader.py
@@ -30,17 +30,11 @@ class Loader(object):
             pprint(self.settings)
 
     def load_settings(self, settings_path):
-        with open(self.settings_path) as f:
-            self.settings = json.load(f)
+        self.settings = load_json(settings_path)
 
     def load_gmt(self, gmt_path):
         if gmt_path:
             self.gmt = GMT(gmt_path)
-
-    def login_synapse(self, overwrite=False):
-        self.syn = synapseclient.login()
-        self.save_settings_to_synapse(self.settings_path,
-                                      overwrite=overwrite)
 
     def load_dataset(self, task):
         ds = self.settings['dataset']
@@ -54,16 +48,30 @@ class Loader(object):
             raise NotImplementedError
 
     def load_processor(self, task):
-        self.settings['processor'] = self.settings["processor"]
-        name = self.settings['processor']["name"]
-        if name == 'LPOCV':
-            if task == "predict":
-                self.proc = LPOCV(settings=self.settings['processor'],
-                                  dataset=self.dataset)
-            elif task == 'score':
-                self.proc = ScoreLPOCV(settings=self.settings)
-            elif task == "stats":
-                self.proc = ScoreLPOCV(settings=self.settings)
+        if task == "predict":
+            self.load_predictor(self)
+        elif task == 'score':
+            self.load_scorer(self)
+        elif task == "stats":
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+    def load_predictor(self):
+        scheme = self.settings['processor']["scheme"]
+        if scheme == 'LPOCV':
+            self.proc = LPOCV(settings=self.settings['processor'],
+                              dataset=self.dataset)
+        else:
+            raise NotImplementedError
+
+    def load_scorer(self):
+        scheme = self.settings['processor']["scheme"]
+        if scheme == 'LPOCV':
+            self.proc = ScoreLPOCV(settings=self.settings)
+        else:
+            raise NotImplementedError
+
         else:
             raise NotImplementedError
 
@@ -74,131 +82,5 @@ class Loader(object):
         to_update = {'dataset.': self.dataset,
                      'processor.': self.proc}
         for prefix, obj in to_update.items():
-            flat_settings = flatten_settings(obj.settings, prefix)
-            self.annotations.update(flat_settings)
             flat_annotations = flatten_settings(obj.annotations, prefix)
             self.annotations.update(flat_annotations)
-
-    def save_settings_to_synapse(self, settings_path, overwrite=False):
-        """Saves prediction to Synapse"""
-        if not self.syn:
-            print('Not logged into synapse.')
-            return
-        parent = get_or_create_syn_folder(self.syn,
-                                          'run_settings/',
-                                          self.settings['project_synid'])
-        localfile = File(path=settings_path, parent=parent)
-        remotefile = get_or_create_syn_entity(localfile, self.syn,
-                                              skipget=False,
-                                              returnid=False)
-        self.get_annotations()
-        md5 = self.annotations['settings_md5']
-        if [md5] == remotefile.annotations.get('settings_md5'):
-            print('Local settings file and remote have the same md5 hashes.')
-        else:
-            print('Local settings file and remote have DIFFERENT md5 hashes.')
-            if overwrite:
-                print('Overwriting remote.')
-                file = self.syn.store(localfile)
-                annotations = {'btr_file_type': 'settings',
-                               'settings_md5': md5}
-                file.annotations = annotations
-                file = get_or_create_syn_entity(file, self.syn,
-                                                skipget=overwrite)
-            else:
-                print('Overwrite disabled. Remote unchanged. Local unchanged.')
-                raise synapseclient.exceptions.SynapseError
-
-    def save_prediction_to_synapse(self):
-        """Saves prediction to Synapse"""
-        if not self.syn:
-            print('Not logged into synapse')
-            return
-        dirpath = self.proc._outdir_path
-        filename = self.proc._outfile_name
-        parent = get_or_create_syn_folder(self.syn,
-                                          dirpath,
-                                          self.settings['project_synid'])
-        file = File(path=dirpath + filename, parent=parent)
-        annotations = get_settings_annotations(self.settings)
-        annotations['btr_file_type'] = 'prediction'
-        if 'background_predictions' in dirpath:
-            annotations['prediction_type'] = 'background'
-        elif "hypothesis_predictions" in dirpath:
-            annotations['prediction_type'] = "hypothesis"
-        else:
-            raise NotImplementedError
-        gmt = self.proc.gmt
-        if gmt:
-            annotations['gmt'] = gmt.suffix
-        file.annotations = annotations
-        file = get_or_create_syn_entity(file, self.syn,
-                                        skipget=True,
-                                        returnid=False)
-
-
-def get_synapse_dict(syn, project_synid):
-    walked = synapseutils.walk(syn, project_synid)
-    contents = [x for x in walked]
-    contents_dict = {}
-    project_name = contents[0][0][0]
-    contents_dict[''] = contents[0][0][1]
-    for c in contents:
-        base = c[0]
-        folders = c[1]
-        files = c[2]
-        for folder in folders:
-            folder_path = "/".join([base[0], folder[0], ''])
-            folder_path = folder_path.replace(project_name + '/', '')
-            syn_id = folder[1]
-            contents_dict[folder_path] = syn_id
-        for file in files:
-            file_path = "/".join([base[0], file[0]])
-            file_path = file_path.replace(project_name + '/', '')
-            syn_id = file[1]
-            contents_dict[file_path] = syn_id
-    return contents_dict
-
-
-def get_or_create_syn_folder(syn, dirpath, project_synid, max_attempts=10,
-                             create=True):
-    dirs = [x for x in dirpath.split('/') if len(x) > 0]
-    folder_synid = project_synid
-    for d in dirs:
-        folder = Folder(d, parent=folder_synid)
-        folder_synid = get_or_create_syn_entity(folder, syn)
-    return folder_synid
-
-
-def get_or_create_syn_entity(entity, syn, max_attempts=10,
-                             create=True, skipget=False, returnid=True):
-    attempts = 1
-    while attempts <= max_attempts:
-        try:
-            if skipget:
-                print('Writing without checking for entity.')
-                raise TypeError
-            else:
-                print('Attempting to get entity "' + entity.name + '".')
-                entity = syn.get(entity)
-                entity_synid = entity.id
-                print('Entity "' + entity.name + '" found.')
-                break
-        except TypeError:
-            try:
-                print('Attempting to create entity.')
-                if create:
-                    entity = syn.store(entity)
-                    entity_synid = entity.id
-                    break
-                else:
-                    print('Create set to False. Entity not created.')
-                    break
-            except synapseclient.exceptions.SynapseHTTPError:
-                print('SynapseHTTPError. Retrying. Attempt ' + attempts)
-                attempts += 1
-                continue
-    if returnid:
-        return entity_synid
-    else:
-        return entity
