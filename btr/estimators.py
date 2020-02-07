02198afb7fcb304143259ab1bf7d673e46d2b19c
pvtodorov <petar.v.todorov@gmail.com>
2020-02-07 20:35:23 +0100

    Make RidgeClassifier output more like probabilities
    As described in Returns the distance from the hyperplane of the
    RidgeClassifer. Not a probability, per se, but values are still rankable for
    LPOCV.
    
            Probability for a pair looks like this:
            array([[0.56079167, 0.43920833],
                   [0.562586  , 0.437414  ]])
    
    The decision function produces distance from hyperplane for each
    sample. predict_proba usually produces one set of probabilities per
    sample
    [p_class_A, p_class_B] where p_class_A = 1-p_class_B
    
    To produce something similar to this, negate the distances.
    Append negated values, and return transposed array.
    [dist_hyperplane, -dist_hyperplane]

--- btr/estimators.py
+++ btr/estimators.py
@@ -3,6 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import RidgeClassifier
 from xgboost import XGBClassifier
+import numpy as np
+
 
 
 def get_estimator(settings):
@@ -30,6 +32,19 @@ class RidgeClassifierBTR(RidgeClassifier):
     def predict_proba(self, X_test):
         """Returns the distance from the hyperplane of the RidgeClassifer.
         Not a probability, per se, but values are still rankable for LPOCV.
+
+        Probability for a pair looks like this:
+        array([[0.56079167, 0.43920833],
+               [0.562586  , 0.437414  ]])
+
+        The decision function produces distance from hyperplane for each sample.
+        predict_proba usually produces one set of probabilities per sample
+        [p_class_A, p_class_B] where p_class_A = 1-p_class_B
+
+        To produce something similar to this, negate the distances.
+        Append negated values, and return transposed array.
+        [dist_hyperplane, -dist_hyperplane]
         """
         probas = self.decision_function(X_test)
+        probas = np.transpose(np.array([probas, -probas]))
         return probas
